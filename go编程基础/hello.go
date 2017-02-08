5fbcec8208d96de6f5cf8e3d1ad2a39b61d865b2
eagle <uttflying@foxmail.com>
2017-02-08 16:48:43 +0800

    go 基础

--- go编程基础/hello.go
+++ go编程基础/hello.go
@@ -2,13 +2,53 @@ package main
 
 import (
 	"fmt"
+//	"os"
 )
+
+/*
+const (
+	i = 100
+	pi =3.1415
+	prefix  = "GO_"
+)
+
+var (
+	i int
+	pi float32
+	prefix string
+)
+*/
+
+const (
+	x = iota //x==0
+	y = iota //y==1
+	z = iota //z==2
+	w        //w==3
+)
+
+const v  =  iota //v==0
+const (
+	h,i,j = iota,iota,iota //h=0,i=o,j=0
+)
+const(
+	a	= iota //a=0
+	b	= "B"
+	c	= iota
+	d,e,f =iota,iota,iota //d=3,e=3,f=3
+	g	= iota  //g =4
+)
+
 func main() {
-	h := "hello"
+	h := "hello\t"
 	w := "go"
-	a := h + w
+	hw := h + w
 	fmt.Println("hello ,go!2017")
-	fmt.Println("%s\n",a)
+	fmt.Println("%s\n",hw)
+	a := `hello
+	go
+	eagle!`
+	//fmt.Printf(a)
+	fmt.Println(a,b,c,d,e,f,g,h,i,j,x,y,z,w,v)
 }
 
 // test go
